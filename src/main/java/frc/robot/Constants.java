9b2e39da5ce383b754dd0d19be0315b490995e07
Shaurya Todani <shauryatodani@gmail.com>
2026-01-25 12:19:05 -0500

    Added feederconfig, launcherconfig, and added their motor current limits to the constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,5 +9,8 @@ public final class Constants {
         // Motor controller IDs
         public static final int FEEDER_MOTOR_ID = 1;
         public static final int INTAKE_LAUNCHER_MOTOR_ID = 2;
+        // Current limit constants
+        public static final int FEEDER_MOTOR_CURRENT_LIMIT = 60;
+        public static final int LAUNCHER_MOTOR_CURRENT_LIMIT = 60;
     }
 }
