c9faacd591cc4c46df1f331d09fb7ebc9dd70754
Shaurya Todani <shauryatodani@gmail.com>
2026-01-31 15:47:11 -0500

    Added kinematics for swerve drive

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -4,14 +4,25 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.DriveConstants.*;
 
 public class Drive extends SubsystemBase {
+  SwerveDriveKinematics swerveKinematics;
   /** Creates a new Drive. */
-  public Drive() {}
+  public Drive() {
+    swerveKinematics = new SwerveDriveKinematics(
+      LEFT_FRONT_SWERVE_MODULE_POSITION,
+      RIGHT_FRONT_SWERVE_MODULE_POSITION,
+      LEFT_BACK_SWERVE_MODULE_POSITION,
+      RIGHT_BACK_SWERVE_MODULE_POSITION
+    );
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 }
+
